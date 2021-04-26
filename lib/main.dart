221ea9a8fd6985353259e02b4a9a429d6bd7b42b
arman heidari <armanartgallery@gmail.com>
2021-04-26 18:13:00 +0430

    Refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:chfcommon/Text/SignUpText.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,28 +12,6 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Container(
-            height: 300,
-            width: 300,
-            child: Column(
-              children: [
-                TitleText("Password", 14),
-                HintText("Your password\n"),
-                Row(
-                  children: [
-                    GrayText("Have an account?   ", 0xff989EB1, 16),
-                    TitleText("SignUp", 16),
-                  ],
-                ),
-                SignInUpText("\nSignUp\n\nSignIn")
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+    return Container();
   }
 }
